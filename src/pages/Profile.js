83b60643514af213e822c7dd426be80875aff7f8
Thikhina Bakmeedeniya <thikhinab@gmail.com>
2021-06-13 14:54:58 +0800

    profile page template

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,6 +3,8 @@ import React, {useContext, useEffect, useState} from 'react'
 import { Redirect } from 'react-router-dom'
 import NavigationBar from '../components/NavigationBar'
 import { UserContext } from '../utils/UserContext'
+// Demo
+import pic from '../demo/profile.jpg'
 
 const Profile = () => {
 
@@ -38,11 +40,66 @@ const Profile = () => {
     }
 
     return (
-        <div>
-            <NavigationBar loggedin={user} func={() => {
-                localStorage.removeItem('prosfero-token')
-                setUser(null)}}/>
-            <div className='profile-box'>
+        <>
+            
+            <NavigationBar loggedin={user} func={() => 
+                {
+                    localStorage.removeItem('prosfero-token')
+                    setUser(null)
+                }
+            }/>
+            <div className='profile-background'>
+                <div className="container profile">
+                    <div className="row">
+                        <div className="col">
+                            <div className="text-center" style={{fontFamily: 'Dancing Script'}}>
+                                <h2>About you</h2>
+                            </div>
+                            <div className="profile-box text-center">
+                                <div>
+                                    <h6 style={{fontWeight: 'bold'}}>Name</h6>
+                                    <p>David Spade</p>
+                                </div>
+                                <div>
+                                    <h6 style={{fontWeight: 'bold'}}>Email</h6>
+                                    <p>example@example.com</p>
+                                </div>
+                                <div>
+                                    <h6 style={{fontWeight: 'bold'}}>Date Jonined</h6>
+                                    <p>25th July 2021</p>
+                                </div>
+                            </div>                          
+                        </div>
+                        <div className="col text-center" style={{margin: '1rem'}}>
+                            <img className='profile-picture' src={pic} alt='profile pif'/>
+                            <h2 style={{fontFamily:'Dancing Script', marginTop: '1rem'}}>David23</h2>
+                        </div>
+                        <div className="col">
+                            <div className="text-center" style={{fontFamily: 'Dancing Script'}}>
+                                <h2>Stats</h2>
+                            </div>
+                            <div className="profile-box text-center">
+                                <div>
+                                    <h6 style={{fontWeight: 'bold'}}>Posts</h6>
+                                    <p>10</p>
+                                </div>
+                                <div>
+                                    <h6 style={{fontWeight: 'bold'}}>Requests</h6>
+                                    <p>2</p>
+                                </div>
+                                <div>
+                                    <h6 style={{fontWeight: 'bold'}}>Achievement Level</h6>
+                                    <p>Newbie</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+                
+{/*             <div className='profile-box'>
                 <h1 style={{fontFamily: 'Dancing Script', fontSize: '3rem'}}>{user.username}</h1>
                 <p style={{fontFamily: 'Dancing Script', fontSize: '1.5rem'}}>Your profile</p>
                 <br />
@@ -53,8 +110,8 @@ const Profile = () => {
                     <li className="list-group-item"><span style={{fontWeight: 'bold'}} >Posts:</span> 0</li>
                     <li className="list-group-item"><span style={{fontWeight: 'bold'}} >Achievement Level:</span> NEWBIE</li>
                 </ul>
-            </div>
-        </div>
+            </div> */}
+        </>
     )
 }
 
